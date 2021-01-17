47df69efa07b45dcba14e424a461ad8e4daee259
전유진 <wjsdbwls7777@gmail.com>
2021-01-17 20:46:59 +0900

    MVC Pattern Part 1

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ import morgan from 'morgan'; // [middleware] morgan: 로그 기록 시 사용
 import helmet from 'helmet'; // [middleware] helmet: 보안
 import cookieParser from 'cookie-parser'; // [middleware] cookie-parser: session을 다룰 시 사용
 import bodyParser from 'body-parser'; // [middleware] body-parser: form 데이터를 서버로 받아와서 활용
-import { userRouter } from './router'; // 사용자 라우터
+import userRouter from './routers/userRouter'; // 사용자 라우터
+import videoRouter from './routers/videoRouter'; // 비디오 라우터
+import globalRouter from './routers/globalRouter'; // 전역 라우터
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello From Home.');
-
-const handleProfile = (req, res) => res.send('You are on my profile.');
 
 /** 
  * middleware 사용 예시: 사용자의 로그인 여부 판단, 파일 전송 시 중간에서 개입해서 업로드, 로그 기록 etc...
@@ -20,11 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(helmet());
 
-
-app.get('/', handleHome);
-
-app.get('/profile', handleProfile);
-
+app.use('/', globalRouter);
 app.use('/user', userRouter);
+app.use('/video', videoRouter);
 
 export default app;
