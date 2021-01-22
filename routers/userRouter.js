5b50750f8eaed1712c5b3bd35177c5584f32bfe6
Yujin Jeon <wjsdbwls7777@gmail.com>
2021-01-22 10:21:22 +0900

    recap

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import { changePassword, editProfile, join, login, logout, userDetail, users } from '../controllers/userController';
 import routes from '../routes';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send('Users'));
-userRouter.get(routes.userDetail, (req, res) => res.send('User Detail'));
-userRouter.get(routes.changePassword, (req, res) => res.send('Change password'));
+userRouter.get(routes.join, join);
+userRouter.get(routes.login, login);
+userRouter.get(routes.logout, logout);
+userRouter.get(routes.users, users);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
