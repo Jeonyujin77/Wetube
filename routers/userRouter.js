a32ce4af0ae6737a7a19cc1a37966629dc382240
전유진 <wjsdbwls7777@gmail.com>
2021-01-24 19:20:24 +0900

    Home Controller

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import { changePassword, editProfile, join, login, logout, userDetail, users } from '../controllers/userController';
+import { changePassword, editProfile, userDetail, users } from '../controllers/userController';
 import routes from '../routes';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.join, join);
-userRouter.get(routes.login, login);
-userRouter.get(routes.logout, logout);
 userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
