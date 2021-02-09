a622808241ac4c8e6d384ef49bc21859c0940004
전유진 <wjsdbwls7777@gmail.com>
2021-02-09 22:08:00 +0900

    Join Controller

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import morgan from 'morgan'; // [middleware] morgan: 로그 기록 시 사용
 import helmet from 'helmet'; // [middleware] helmet: 보안
-import csp from "helmet-csp";
 import cookieParser from 'cookie-parser'; // [middleware] cookie-parser: session을 다룰 시 사용
 import bodyParser from 'body-parser'; // [middleware] body-parser: form 데이터를 서버로 받아와서 활용
 import userRouter from './routers/userRouter'; // 사용자 라우터
@@ -15,16 +14,10 @@ const app = express();
 /** 
  * middleware 사용 예시: 사용자의 로그인 여부 판단, 파일 전송 시 중간에서 개입해서 업로드, 로그 기록 etc...
  * 주의점: 순서에 따라 결과가 달라지므로 작성순서가 중요하다.
-*/;
-app.use(helmet());
-app.use(
-  csp({
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'"],
-      scriptSrc: ["'self'"],
-    },
-  })
+*/
+app.use(helmet({
+    contentSecurityPolicy: false,
+})
 );
 app.set('view engine', 'pug'); 
 app.use(cookieParser());
