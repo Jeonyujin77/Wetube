2aa1ddb009a8c95bed7d8ca244544bb0d9d4dd9f
전유진 <wjsdbwls7777@gmail.com>
2021-02-09 23:22:39 +0900

    More Controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
 import routes from '../routes';
 
+// 회원가입-get
 export const getJoin = (req, res) => { 
     res.render('join', { pageTitle: 'Join'});
 };
 
+// 회원가입-post
 export const postJoin = (req, res) => {
     const {
         body: {name, email, password, password2}
@@ -21,13 +23,28 @@ export const postJoin = (req, res) => {
     res.render('join', { pageTitle: 'Join'});
 };
 
+// 로그인-get
 export const getLogin = (req, res) => res.render('login', { pageTitle: 'Login'});
 
+// 로그인-post
 export const postLogin = (req, res) => {
     res.redirect(routes.home);
 }
-export const logout = (req, res) => res.render('logout', { pageTitle: 'Logout'});
+
+// 로그아웃
+export const logout = (req, res) => {
+    // To Do: Process Log Out
+    res.redirect(routes.home);
+}
+
+// 사용자
 export const users = (req, res) => res.render('users', { pageTitle: 'Users'});
+
+// 사용자 상세
 export const userDetail = (req, res) => res.render('userDetail', { pageTitle: 'User Detail'});
+
+// 사용자 수정
 export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'Edit Profile'});
+
+// 비밀번호 변경
 export const changePassword = (req, res) => res.render('changePassword', { pageTitle: 'Change Password'});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,12 @@
 import { videos } from '../db';
+import routes from '../routes';
 
+// 메인
 export const home = (req, res) => {
     res.render('home', { pageTitle: 'Home', videos });
 };
 
+// 검색
 export const search = (req, res) => {
     const {
         query: {term: searchingBy}
@@ -11,10 +14,22 @@ export const search = (req, res) => {
     res.render('search', { pageTitle: 'Search', searchingBy, videos});
 };
 
-export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload'});
+// 업로드
+export const getUpload = (req, res) => res.render('upload', { pageTitle: 'Upload'});
 
+export const postUpload = (req, res) => {
+    const {
+        body: {file, title, description}
+    } = req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(324393))
+}
+
+// 비디오 상세
 export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detail'});
 
+// 비디오 수정
 export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'Edit Video'});
 
+// 비디오 삭제
 export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video'});
